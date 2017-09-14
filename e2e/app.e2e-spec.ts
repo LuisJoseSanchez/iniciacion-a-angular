9c47a28a0974a8bf30a118d013b79e881d5b2add
Luis José Sánchez <luisjose@espiral.io>
2017-09-14 11:56:37 +0200

    Añadido el proyecto limpio de Angular.

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('iniciacion-a-angular App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
